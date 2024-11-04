810828383201c58015116b255197336acd6a1acb
Deni Ivančok <cukarakika@gmail.com>
2024-11-04 15:56:25 +0100

    Dodan pozdrav

---
+++ ConsoleApp-RUAP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp_RUAP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Denija Ivančoka");
+        }
+    }
+}
